2d992eeaadfac666ae6386848fa8c99ae75aa44f
Axel <axel.guinot.astro@gmail.com>
2021-12-06 12:04:04 +0100

    Add setup.py
    Just something for convenience

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+setup(
+    name="metacoadd",
+    version="0.0.1",
+    author="Axel Guinot",
+    author_email="axel.guinot.astro@gmail.com",
+    description="Shear image and coadd them to run metadetection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aguinot/metacoadd",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
