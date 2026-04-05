bdd766867d9c993bf29bdffc11b4e2835cf3b23d
aguinot <axel.guinot.astro@gmail.com>
2026-04-05 04:09:26 -0400

    Fix init imports

--- metacoadd/__init__.py
+++ metacoadd/__init__.py
@@ -1,6 +1,6 @@
-from . import detect
-from . import exposure_bound
-from . import exposure
-# from . import metacoadd 
-from . import utils
-from . import moments
+# from . import detect
+# from . import exposure_bound
+# from . import exposure
+# # from . import metacoadd
+# from . import utils
+# from . import moments
